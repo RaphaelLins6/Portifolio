ea6b523c65967b2c03943e9b07289ca97f68fc41
Raphael Lins <rapharell06@gmail.com>
2025-03-10 03:15:35 -0300

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menuLinks = document.querySelector('.menu-links');
 
-     menuToggle.addEventListener('click', function() {
-         menuLinks.classList.toggle('show');
-    });
-});
